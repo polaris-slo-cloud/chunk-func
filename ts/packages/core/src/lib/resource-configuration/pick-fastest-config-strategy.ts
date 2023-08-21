48e37703396851aa3ffd28740a4750201cfd1b1a
Thomas Pusztai <11061338+tommazzo89@users.noreply.github.com>
2023-08-21 15:41:25 +0200

    Add ResourceConfigurationStrategyBase

--- ts/packages/core/src/lib/resource-configuration/pick-fastest-config-strategy.ts
+++ ts/packages/core/src/lib/resource-configuration/pick-fastest-config-strategy.ts
@@ -1,5 +1,6 @@
 import { ResourceProfile, getResultsForInput } from '../model';
-import { AccumulatedStepInput, ChooseConfigurationStrategyFactory, ResourceConfigurationStrategy, WorkflowGraph, WorkflowState, WorkflowFunctionStep } from '../workflow';
+import { AccumulatedStepInput, ChooseConfigurationStrategyFactory, WorkflowGraph, WorkflowState, WorkflowFunctionStep } from '../workflow';
+import { ResourceConfigurationStrategyBase } from './resource-configuration-strategy.base';
 
 export const createPickFastestConfigStrategy: ChooseConfigurationStrategyFactory =
     (graph: WorkflowGraph, availableResProfiles: Record<string, ResourceProfile>) => new PickFastestConfigStrategy(graph, availableResProfiles);
@@ -7,34 +8,29 @@ export const createPickFastestConfigStrategy: ChooseConfigurationStrategyFactory
 /**
  * ResourceConfigurationStrategy that always picks the fastest resource configuration, irrespective of the SLO.
  */
-export class PickFastestConfigStrategy implements ResourceConfigurationStrategy {
+export class PickFastestConfigStrategy extends ResourceConfigurationStrategyBase {
 
     static readonly strategyName = 'PickFastestConfigStrategy';
 
-    readonly name = PickFastestConfigStrategy.strategyName;
-    readonly workflowGraph: WorkflowGraph;
-    readonly availableResourceProfiles: Record<string, ResourceProfile>;
-
     constructor(graph: WorkflowGraph, availableResProfiles: Record<string, ResourceProfile>) {
-        this.workflowGraph = graph;
-        this.availableResourceProfiles = availableResProfiles;
+        super(PickFastestConfigStrategy.strategyName, graph, availableResProfiles);
     }
 
     chooseConfiguration(workflowState: WorkflowState, step: WorkflowFunctionStep, input: AccumulatedStepInput): ResourceProfile {
         let fastestTime = Number.POSITIVE_INFINITY;
-        let fastestProfileId: string | undefined;
+        let selectedProfileId: string | undefined;
 
         for (const resultForInput of getResultsForInput(step.profilingResults, input.totalDataSizeBytes)) {
             if (resultForInput.result.executionTimeMs < fastestTime) {
                 fastestTime = resultForInput.result.executionTimeMs;
-                fastestProfileId = resultForInput.resourceProfileId;
+                selectedProfileId = resultForInput.resourceProfileId;
             }
         }
 
-        if (!fastestProfileId) {
+        if (!selectedProfileId) {
             throw new Error('ProfilingResults did not contain any results.');
         }
-        const profile = this.availableResourceProfiles[fastestProfileId];
+        const profile = this.availableResourceProfiles[selectedProfileId];
         return profile;
     }
 
