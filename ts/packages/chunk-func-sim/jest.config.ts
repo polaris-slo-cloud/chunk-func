ca9e273541b9b368812d2274cf33254976a61032
Thomas Pusztai <11061338+tommazzo89@users.noreply.github.com>
2023-08-31 10:59:55 +0200

    Rename simulator to chunk-func-sim

--- ts/packages/chunk-func-sim/jest.config.ts
+++ ts/packages/chunk-func-sim/jest.config.ts
@@ -1,11 +1,11 @@
 /* eslint-disable */
 export default {
-    displayName: 'simulator',
+    displayName: 'chunk-func-sim',
     preset: '../../jest.preset.js',
     testEnvironment: 'node',
     transform: {
         '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
     },
     moduleFileExtensions: ['ts', 'js', 'html'],
-    coverageDirectory: '../../coverage/packages/simulator',
+    coverageDirectory: '../../coverage/packages/chunk-func-sim',
 };
