9cff8a426d009e9f8e6731468a2e44476d936331
Thomas Pusztai <11061338+tommazzo89@users.noreply.github.com>
2024-01-12 21:30:35 +0100

    Use  only input size to designate scenario to ease CSV processing

--- ts/packages/results-converter/src/main.ts
+++ ts/packages/results-converter/src/main.ts
@@ -42,7 +42,7 @@ function processResultFile(filePath: string): ProcessedSimulationResult {
 
     return {
         strategy: simResult.resourceConfigStrategy.replace('ConfigStrategy', ''),
-        scenario: `${inputSize}MB ${sloSec}s`,
+        scenario: `${inputSize}MB`,
         sloMs: simResult.sloMs,
         executionTimeMs: simResult.workflowOutput.executionTimeMs,
         cost: simResult.workflowOutput.totalCost,
