2b600281b4c9167fd07786365b5396eb07de85b8
Thomas Pusztai <11061338+tommazzo89@users.noreply.github.com>
2024-02-02 19:00:38 +0100

    Account for multiple suggestions mapping to the same integer

--- py/bayesian_optimizer/bayesian_opt/integer_bayesian_optimizer.py
+++ py/bayesian_optimizer/bayesian_opt/integer_bayesian_optimizer.py
@@ -1,5 +1,6 @@
 from dataclasses import dataclass
 from bisect import bisect_left
+import logging
 from typing import cast, TypedDict
 
 from bayes_opt import UtilityFunction
@@ -41,6 +42,10 @@ class IntegerBayesianOptimizer:
         self.__xInterval: IntegerInterval | None = None
         bounds = self.__determineInputBounds(possible_x_values)
 
+        # Since BO suggests floating point values for X, we might get multiple suggestions that map to the same integer X'.
+        # If we already have an observed value for the integer X', we register that value for the suggested float X.
+        self.__observed_values: dict[int, float] = {}
+
         if kappa is None:
             kappa = 2.576
         if xi is None:
@@ -65,9 +70,9 @@ class IntegerBayesianOptimizer:
         """
         Makes a new suggestion for X and also returns its expected improvement.
         """
+        self.__ensure_input_domain_not_exhausted()
 
-        suggestion = self.__optimizer.suggest(self.__eiFn)
-        x_suggestion = suggestion['x']
+        x_suggestion = self.__compute_new_suggestion()
         poi = 1.0
 
         max_y = self.__optimizer.get_max_y()
@@ -78,7 +83,7 @@ class IntegerBayesianOptimizer:
                 raise AssertionError('Could not compute EI')
             poi = utility_poi.max()
 
-        return BoSuggestion(x=self.__suggestion_space_to_param_domain(x_suggestion), poi=poi)
+        return BoSuggestion(x=x_suggestion, poi=poi)
 
 
     def register_observation(self, x: int, observation: float) -> None:
@@ -87,6 +92,7 @@ class IntegerBayesianOptimizer:
         """
         params = { 'x': self.__param_domain_to_suggestion_space(x) }
         self.__optimizer.register(params=params, target=observation)
+        self.__observed_values[x] = observation
 
 
     def __determineInputBounds(self, possible_x_values: list[int] | IntegerInterval) -> tuple[float, float]:
@@ -98,6 +104,34 @@ class IntegerBayesianOptimizer:
             self.__xInterval = cast(IntegerInterval, possible_x_values)
             return (float(self.__xInterval['lower_bound']), float(self.__xInterval['upper_bound']))
 
+
+    def __compute_new_suggestion(self) -> int:
+        suggestion = self.__optimizer.suggest(self.__eiFn)
+        x_int = self.__suggestion_space_to_param_domain(suggestion['x'])
+        observed = self.__observed_values.get(x_int)
+
+        while observed is not None:
+            logging.info('Suggested float maps to already observed x=%d, registering y=%f', x_int, observed)
+            self.__optimizer.register(params=suggestion, target=observed)
+
+            suggestion = self.__optimizer.suggest(self.__eiFn)
+            x_int = self.__suggestion_space_to_param_domain(suggestion['x'])
+            observed = self.__observed_values.get(x_int)
+
+        return x_int
+
+
+    def __ensure_input_domain_not_exhausted(self) -> None:
+        input_domain_size: int
+
+        if self.__xInterval is not None:
+            input_domain_size = self.__xInterval['upper_bound'] - self.__xInterval['lower_bound'] + 1
+        else:
+            possible_x_values = cast(list[int], self.__possible_x_values)
+            input_domain_size = len(possible_x_values)
+
+        if len(self.__observed_values) == input_domain_size:
+            raise ValueError('Cannot make another suggestion, because values for every possible input X have already been observed.')
 
 
     def __suggestion_space_to_param_domain(self, suggestion: float) -> int:
